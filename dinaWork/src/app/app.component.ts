8521af72df36766855f0244f6b7a82f2b34a8075
Eugen Harchishin <6pAT.ua@gmail.com>
2018-06-01 22:57:17 +0300

    Dina1

---
+++ dinaWork/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  floor: number = 0;
+  room: number = 0;
+  visibility: boolean = true;
+  isBusy: boolean = true;
+  personsOnRooms: any[] = [];
+  personsOnSelectRoom: number;
+
+  constructor() {
+
+  }
+
+  onSubmit() {
+
+    this.personsOnSelectRoom = this.personsOnRooms[this.floor * 30 + this.room];
+    (this.personsOnSelectRoom == 0) ? this.isBusy = false : this.isBusy = true;
+    this.visibility = !this.visibility;
+  }
+  goBack(){
+    this.visibility = !this.visibility;
+  }
+
+  ngOnInit() {
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 30; j++) {
+        this.personsOnRooms.unshift(Math.floor((Math.random() * 10) + 0));
+      }
+    }
+  }
+
+
+}
